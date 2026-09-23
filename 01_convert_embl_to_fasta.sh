ff06b58f799a4d4532be849ad4fd50b7bbe286da
Laura Dean <lldean18@gmail.com>
2026-09-23 17:07:26 +0100

    all the file prep is right but I think the .gz file and the fasta file might not be the same length

---
+++ 01_convert_embl_to_fasta.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+# Laura Dean
+# 23/9/26
+
+
+# I used this website to convert the .embl file to fasta: https://www.bioinformatics.org/sms2/embl_fasta.html
+# and this website to convert it to gff3: https://www.ebi.ac.uk/ena/gff3/converter/
+
+
+# setup env
+srun --partition defq --cpus-per-task 4 --mem 20g --time 08:00:00 --pty bash
+
+# setup software
+source $HOME/.bash_profile
+conda activate circos
+
+# set variables
+PROJECT=Klebsiella_pneumoniae
+ASSEMBLY=/gpfs01/home/mbzlld/data/circos_tradis_plot/CP008827.1.fa
+ANNOTATION=/gpfs01/home/mbzlld/data/circos_tradis_plot/CP008827.1.gff
+TRADIS=/gpfs01/home/mbzlld/data/circos_tradis_plot/trimmed.fq.ENA_CP009273_CP009273.1.insert_site_plot.gz
+
+# setup wkdir
+cd /gpfs01/home/mbzlld/data/circos_tradis_plot
+mkdir -p /gpfs01/home/mbzlld/data/circos_tradis_plot/$PROJECT
+cd /gpfs01/home/mbzlld/data/circos_tradis_plot/$PROJECT
+
+
+
+#####################
+### PREP ASSEMBLY ###
+#####################
+
+# convert assembly to the right format
+# first index it
+conda activate samtools1.24
+samtools faidx $ASSEMBLY
+conda deactivate
+# without naming the chrs with their chr names
+awk '{print "chr - " $1 " " $1 " 0 " $2 " chr1"}' $ASSEMBLY.fai > karyotype.txt
+
+##############################
+### PREP GENOME ANNOTATION ###
+##############################
+
+# convert annotation to circos format
+awk '$3=="CDS"' $ANNOTATION | awk '{print $1, $4, $5}' OFS="\t" > genes.txt
+
+# make separate annotation files for genes on fwd and rev strands (strand info is 7th column)
+# fwd strand
+awk '$3=="CDS" && $7=="+"' $ANNOTATION |
+awk '{print $1, $4, $5}' OFS="\t" > genes_fwd_strand.txt
+# rev strand
+awk '$3=="CDS" && $7=="-"' $ANNOTATION |
+awk '{print $1, $4, $5}' OFS="\t" > genes_rev_strand.txt
+
+################################
+### PREP INSERTION SITE DATA ###
+################################
+
+# make genome windows to count insertion sites in
+bedtools makewindows -g $ASSEMBLY.fai -w 20000 > windows_20kb.bed
+
+# convert the tradis insertion site output to bed format
+zcat "$TRADIS" | awk '
+BEGIN {
+    while ((getline < "'$ASSEMBLY.fai'") > 0) {
+        chr[++n] = $1
+        len[n] = $2
+    }
+    c = 1
+    pos = 0
+}
+{
+    print chr[c], pos, pos+1, $1 >> "insertions_fwd_strand.bed"
+    print chr[c], pos, pos+1, $2 >> "insertions_rev_strand.bed"
+    pos++
+    if (pos >= len[c]) {
+        c++
+        pos = 0
+    }
+}
+' OFS='\t'
+
+# count the insertions per window
+bedtools map -a windows_20kb.bed -b insertions_fwd_strand.bed -c 4 -o sum -null 0 > insertions_fwd_strand_20kb.bed
+bedtools map -a windows_20kb.bed -b insertions_rev_strand.bed -c 4 -o sum -null 0 > insertions_rev_strand_20kb.bed
+
+
+#####################
+### TO RUN CIRCOS ###
+#####################
+# make the circos.conf file stipulating how you want the plot to be then
+# in the dir with the circos.conf file run:
+conda activate circos
+circos
+
+
