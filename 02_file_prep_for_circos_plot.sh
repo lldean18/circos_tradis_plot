186c30233c0b223abe9cfa1108b75c88e8907b0c
Laura Dean <lldean18@gmail.com>
2026-09-24 21:14:11 +0100

    hacing a go at adding oriC to the plot

--- 02_file_prep_for_circos_plot.sh
+++ 02_file_prep_for_circos_plot.sh
@@ -118,7 +118,7 @@ bedtools map -a windows_1kb.bed -b insertions_rev_strand.bed -c 4 -o sum -null 0
 
 # or simpler
 
-#awk '$3=="oriC" {print $1, $4, $5}' OFS="\t" "$ANNOTATION" > oriC.txt
+awk '$3=="oriC" {print $1, $4, $5, "oriC"}' OFS="\t" "$ANNOTATION" > oriC.txt
 
 #####################
 ### TO RUN CIRCOS ###
